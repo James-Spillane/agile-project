6eba6eef2554cd38ef8bd0fe04bfbe0497046477
Cathal Murray <cathal.murray@mycit.ie>
2025-10-14 10:28:59 +0100

    Initial Express skeleton

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.use('/', indexRouter);
+
+// 404
+app.use((req, res) => res.status(404).render('error', { title: 'Not Found' }));
+
+module.exports = app;
