fdc365751adba61cf25d0c1b3cff013f83cc6ea4
cathalmurray9 <150475166+cathalmurray9@users.noreply.github.com>
2025-10-14 12:54:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
-app.use(express.json());                       // <-- needed for POST JSON
+app.use(express.json());                      
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
