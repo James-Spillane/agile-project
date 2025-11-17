5f7d81a127bb43f85fb11f1fbfc42a0cc7b9b12f
Cathal Murray <cathal.murray@mycit.ie>
2025-11-17 14:19:40 +0000

    easter egg

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,9 @@ router.get('/', (req, res) => {
   res.render('index', { title: 'Screen Time Tracker' });
 });
 
+router.get('/mascot', (req, res) => {
+    res.render('mascot');
+});
+
+
 module.exports = router;
